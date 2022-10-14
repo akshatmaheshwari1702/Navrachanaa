52812b3ad8118fd4bc13183ba74d3a58ac48d3ed
Akshat Maheshwari <maheshwariakshat482@gmail.com>
2022-10-14 12:09:27 +0530

    date changed

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,8 +6,8 @@ import Themes from "../components/pages-parts/Themes";
 const About = () => {
   const [state] = useState({
     heading: "About Navarachnaa 2022",
-    pageHeading: "6th National Level Project Competition",
-    message: `Navarachanaa 2022 is 6th National level Project Competition organised by Shri Vaishnav Vidyapeeth Vishwavidyalaya for students of University to showcase their ideas and implementations as a cherishing outcome in the contribution to the Nation's Dream of India under current government programs like Digital India, Make in India, Skill India, Smart City, Swachh Bharat Mission, National Health Mission, Start-up India etc.`
+    pageHeading: "7th National Level Project Competition",
+    message: `Navarachanaa 2022 is 7th National level Project Competition organised by Shri Vaishnav Vidyapeeth Vishwavidyalaya for students of University to showcase their ideas and implementations as a cherishing outcome in the contribution to the Nation's Dream of India under current government programs like Digital India, Make in India, Skill India, Smart City, Swachh Bharat Mission, National Health Mission, Start-up India etc.`
   });
   useEffect(() => {
     window.scrollTo(0, 0);
